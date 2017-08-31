623d4546ac702de4a09098b8e0e4adc71ddec193
Simon Kassel <kassel.simon@gmail.com>
2017-08-31 18:43:10 -0400

    update path

--- docs/build_site.R
+++ docs/build_site.R
@@ -2,7 +2,7 @@
 #This helps avoid confusion if our working directory is 
 #not our site because of other projects we were 
 #working on at the time. 
-setwd("C:/Users/skassel/Desktop/website/predicting-building-inspections/site/")
+setwd("C:/Users/skassel/Desktop/website/predicting-building-inspections/docs/")
 
 #render site. 
 rmarkdown::render_site()
